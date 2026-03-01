1257953e164cb0c75a11450df9b25255859665b8
Ariel Fu <xinyuf3@uw.edu>
2026-02-28 23:50:29 -0800

    viz2 - interactive setup

--- js/sketches/viz/viz_2_tuition.js
+++ js/sketches/viz/viz_2_tuition.js
@@ -1,5 +1,21 @@
 (function () {
     window.Viz2Tuition = {
+        // Data variables
+        table2: null,
+        
+        // UI Control variables
+        measureSelect: null,
+        categorySelect: null,
+        institutionSelect: null,
+
+        // State variables to track changes
+        lastMeasure: '',
+        lastCategory: '',
+        lastInstitution: '',
+        currentDataset: [],
+
+        controlsCreated: false,
+        
         draw: function (p, manager, ai, progress) {
             p.push();
             
