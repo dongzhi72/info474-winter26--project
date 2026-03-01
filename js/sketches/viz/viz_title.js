1617d692cf670633e96c44a1cd704a1d990b82a3
Ariel Fu <xinyuf3@uw.edu>
2026-03-01 02:24:13 -0800

    My latest changes

--- js/sketches/viz/viz_title.js
+++ js/sketches/viz/viz_title.js
@@ -15,7 +15,7 @@
             p.fill(0);
             p.textAlign(p.CENTER, p.CENTER);
             p.textSize(48);
-            // p.text(ai === 0 ? 'INFO 474' : 'Final Project', cx, cy);
+            p.text(ai === 0 ? 'Is College Still Worth It?' : 'Cost, Access, Risk, and Return', cx, cy);
             p.pop();
         }
     };
