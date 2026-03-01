18e29849447bfb864b97fcd622126255e7c73672
Ariel Fu <xinyuf3@uw.edu>
2026-02-28 23:52:59 -0800

    viz2 - implement draw function

--- js/sketches/viz/viz_2_tuition.js
+++ js/sketches/viz/viz_2_tuition.js
@@ -19,9 +19,27 @@
         draw: function (p, manager, ai, progress) {
             p.push();
             
+            // --- 1. SETUP UI CONTROLS (Once) ---
+            if (!this.controlsCreated) {
+                this.createControls(p, manager);
+                this.controlsCreated = true;
+            }
+
+            // --- 2. HANDLE UPDATING VIS STATE (Filtering) ---
+            this.handleResetVisState(manager);
+
+            // --- 3. RENDER THE CHART ---
+            if (this.currentDataset.length > 0) {
+                this.renderChart(p, manager);
+            } else {
+                p.fill(100);
+                p.textAlign(p.CENTER, p.CENTER);
+                p.text("No data found for selection.", manager.width / 2, manager.height / 2);
+            }
             
             
             p.pop();
         }
+        
     };
 })();
