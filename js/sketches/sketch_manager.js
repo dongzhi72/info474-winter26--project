857751b85126798058f92e2e1c8f1cd2333d2f7f
Ariel Fu <xinyuf3@uw.edu>
2026-03-02 17:18:39 -0800

    viz3: load dataset, set slider variables

--- js/sketches/sketch_manager.js
+++ js/sketches/sketch_manager.js
@@ -30,6 +30,7 @@ function startP5() {
                 // This loads the CSV and attaches it to the manager (self)
                 self.table1 = p.loadTable('data/df1_college_enrollment_1960_2022_long.csv', 'csv', 'header');
                 self.table2 = p.loadTable('data/df2_tuition_full_long.csv', 'csv', 'header');
+                self.table3 = p.loadTable('data/df3_state_enroll_long.csv', 'csv', 'header')
                 self.table4 = p.loadTable('data/df4_race_enrollment.csv', 'csv', 'header');
             };
 
--- js/sketches/viz/viz_3_state.js
+++ js/sketches/viz/viz_3_state.js
@@ -1,11 +1,14 @@
 (function () {
     window.Viz3State = {
+        yearSlider: null,
+        sliderLabel: null,
+        controlsCreated: false,
+        stateAverages: {},
+        maxEnrollment: 0,
+        availableYears: [1970, 1980, 1990, 2000, 2010, 2012, 2017, 2018, 2019, 2020, 2021, 2022],
+
         draw: function (p, manager, ai, progress) {
-            p.push();
             
-            
-            
-            p.pop();
         }
     };
 })();
